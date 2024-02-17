35864c0de1fc39d1cd2c366aea3f0e87545bcd19
chikki30 <charan991991@gmail.com>
2024-02-17 14:52:58 -0500

    BasicArrays

---
+++ Findingsumofelements.java
@@ -0,0 +1,12 @@
+public class Findingsumofelements {
+    public static void main(String[] args) {
+        int A[] = {3, 9, 7, 8, 12, 6, 15, 5, 4, 10};
+        int sum = 0;
+
+        for (int x : A) {
+            sum = sum + x;
+        }
+
+        System.out.println("sum is" + sum);
+    }
+}
